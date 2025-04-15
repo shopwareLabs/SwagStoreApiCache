f314323b59844f8dc61abb402223a4840f4f87b6
Jan Schöpke <janschoepke@ymail.com>
2025-04-15 19:00:38 +0200

    added individual route support via plugin config, added debug command

--- src/SwagStoreAPICache.php
+++ src/SwagStoreAPICache.php
@@ -8,7 +8,38 @@ use Shopware\Core\Framework\Plugin\Context\DeactivateContext;
 use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 use Shopware\Core\Framework\Plugin\Context\UpdateContext;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 
 class SwagStoreAPICache extends Plugin
 {
+    public const CONFIG_KEY = 'SwagStoreAPICache.config.additionalCacheableRoutes';
+
+    public function install(InstallContext $installContext): void
+    {
+        parent::install($installContext);
+    }
+
+    public function update(UpdateContext $updateContext): void
+    {
+        parent::update($updateContext);
+    }
+
+    public function activate(ActivateContext $activateContext): void
+    {
+        parent::activate($activateContext);
+    }
+
+    public function deactivate(DeactivateContext $deactivateContext): void
+    {
+        parent::deactivate($deactivateContext);
+    }
+
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+        if ($uninstallContext->keepUserData()) {
+            return;
+        }
+
+        parent::uninstall($uninstallContext);
+    }
 }
